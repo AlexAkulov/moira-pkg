f66fa3c325d27b662469837c1eb7b5e409ccf509
alexander akulov <akulov@skbkontur.ru>
2016-01-12 16:19:35 +0500

    add cheks

--- build-moira.sh
+++ build-moira.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
 
 HELP="
-This script can be created deb/rpm packages, doker image of tar archive
-for Moira monitoring last version from https://github.com/moira-alert
+This script can be create deb/rpm packages or doker image
+contained last version for Moira monitoring system (https://github.com/moira-alert)
 
-Requirements: Golang and fpm
+Requirements: Golang, fpm, rpmbuild (for rpm), dpkg (for deb), docer (for docker)
 
 Usage:
    ./build.sh MICROSERVICE PACKAGETYPE
-   MICROSERVICE must be notifier|cache|web|worker|api|full|all
-   PACKAGETYPE must be deb|rpm|tar|docker|all
+   MICROSERVICE must be notifier|cache|web|worker|full|all
+   PACKAGETYPE must be deb|rpm|docker|all
 
 Examples:
    ./build.sh notifier rpm - created rpm packages contained last version of moira-notifier
-   ./build.sh full docker - created docker image contained redia and full moira monitoring ready to working
+   ./build.sh full docker - created docker image contained redis and full moira monitoring ready to working
 
 "
 #set -e
 URL="https://github.com/moira-alert"
-LICENSE="GNU GPL v3"
+LICENSE="GPLv3"
 VENDOR="SKB Kontur"
 MAINTEINER="https://gitter.im/moira-alert/moira"
 MICROSERVICE=$1
 PACKAGETYPE=$2
 PARAMS=$#
-GOROOT="$(go env GOROOT)" # default golang
-#GOROOT=/usr/local/go1.5.2
-#GOROOT=/usr/local/go1.4.2
-GOPATH="$(pwd)/gocode"
+DIST="$(pwd)/dist"
+TMP="$(pwd)/tmp"
+
+GOBIN="$(which go 2>/dev/null)" # default golang
+#GOROOT=/usr/local/go1.5.2/bin/go
+#GOROOT=/usr/local/go1.4.2/bin/go
+GOPATH="$TMP/gocode"
 
 check() {
   if [ "$PARAMS" -ne 2 ]; then
     echo "ERROR! Are expected two params"
     print_help
   fi
-  if ! [ -x "$GOROOT/bin/go" ]; then
+  if ! [ -x "$GOBIN" ]; then
     echo "Golang must be installed"
     exit 1
   fi
@@ -42,28 +45,58 @@ check() {
     echo "FPM must be installed"
     exit 1
   fi
-#    if ! [ -x "$(which docker 2>/dev/null)" ]; then
-#      echo "Docker mmust be installed"
-#      exit 1
-#    fi
-# rpmbuild?
-
+  case "$MICROSERVICE" in
+  "notifier"|"cache"|"web"|"worker"|"full"|"all")
+  ;;
+  *)
+    echo "ERROR! Undefined Microservice"
+    print_help
+  ;;
+  esac
+  case "$PACKAGETYPE" in
+  "rpm")
+    if ! [ -x "$(which rpmbuild 2>/dev/null)" ]; then
+      echo "rpmbuild not found "
+    fi
+  ;;
+  "deb")
+    if ! [ -x "$(which dpkg 2>/dev/null)" ]; then
+      echo "dpkg not found "
+    fi
+  ;;
+  "docker")
+    if ! [ -x "$(which docker 2>/dev/null)" ]; then
+      echo "docker not found "
+    fi
+  ;;
+  "all")
+    for tool in "rpmbuild" "dpkg" "docker"; do
+      if ! [ -x "$(which $tool 2>/dev/null)" ]; then
+        echo "$tool not found "
+      fi
+    done
+  ;;
+  *)
+    echo "ERROR! Undefined Package type"
+    print_help
+  ;;
+  esac
 }
 
 clean() {
   echo "Cleaning"
-  rm -rf ./dist
+  rm -rf $DIST
 # rm -rf ./gocode
-  mkdir ./dist &>/dev/null
+  mkdir $DIST &>/dev/null
   mkdir $GOPATH &>/dev/null
   clean_package
-  rm -rf ./tmp
-  mkdir ./tmp &>/dev/null
+  #rm -rf $TMP
+  mkdir $TMP &>/dev/null
 }
 
 clean_package() {
-  rm -rf ./package
-  mkdir ./package &>/dev/null
+  rm -rf $TMP/package
+  mkdir $TMP/package &>/dev/null
 }
 
 print_help() {
@@ -73,17 +106,17 @@ print_help() {
 
 setup_worker() {
   echo "Install worker"
-  git clone "https://github.com/moira-alert/worker" ./tmp/worker
+  git clone "https://github.com/moira-alert/worker" $TMP/worker
 
 }
 
 setup_notifier() {
   echo "Install dependencies for moira-notifier"
-  $GOROOT/bin/go get -v "github.com/moira-alert/notifier"
-  $GOROOT/bin/go get -v "github.com/moira-alert/notifier/notifier"
+  $GOBIN get -v "github.com/moira-alert/notifier"
+  $GOBIN get -v "github.com/moira-alert/notifier/notifier"
 
-  #$GOROOT/bin/go build -v "github.com/moira-alert/notifier/notifier"
-  $GOROOT/bin/go install -v "github.com/moira-alert/notifier/notifier"
+  #$GOBIN build -v "github.com/moira-alert/notifier/notifier"
+  $GOBIN install -v "github.com/moira-alert/notifier/notifier"
 }
 
 package_notifier() {
@@ -92,21 +125,21 @@ package_notifier() {
   NOTIFIER_DATE_COMMIT=$(date -d $(git --git-dir $GOPATH/src/github.com/moira-alert/notifier/.git log --date=short --pretty=format:"%cd" -n 1) +%Y%m%d)
   NOTIFIER_VERSION="$NOTIFIER_DATE_COMMIT$NOTIFIER_COMMIT"
 
-  mkdir -p ./package/etc/init.d
-  mkdir -p ./package/etc/moira
-  mkdir -p ./package/usr/sbin
-  mkdir -p ./package/usr/lib/systemd/system
+  mkdir -p $TMP/package/etc/init.d
+  mkdir -p $TMP/package/etc/moira
+  mkdir -p $TMP/package/usr/sbin
+  mkdir -p $TMP/package/usr/lib/systemd/system
 
-  cp ./files/notifier/$PT/init.d/moira-notifier ./package/etc/init.d/moira-notifier
-  cp ./files/notifier/$PT/systemd/moira-notifier.service ./package/usr/lib/systemd/system/moira-notifier.service
-  cp ./files/config.yml ./package/etc/moira/config.yml
-  cp $GOPATH/bin/notifier ./package/usr/sbin/moira-notifier
+  cp ./files/notifier/$PT/init.d/moira-notifier $TMP/package/etc/init.d/moira-notifier
+  cp ./files/notifier/$PT/systemd/moira-notifier.service $TMP/package/usr/lib/systemd/system/moira-notifier.service
+  cp ./files/config.yml $TMP/package/etc/moira/config.yml
+  cp $GOPATH/bin/notifier $TMP/package/usr/sbin/moira-notifier
 
   echo "Create $PT package"
   fpm -t $PT \
     -s "dir" \
     --description "Moira Notifier" \
-    -C ./package \
+    -C $TMP/package \
     --vendor "$VENDOR" \
     --url "$URL" \
     --license "$LICENSE" \
@@ -117,13 +150,13 @@ package_notifier() {
     --config-files "/etc/moira/config.yml" \
     --config-files "/usr/lib/systemd/system/moira-notifier.service" \
     --after-install "./files/notifier/$PT/postinst" \
-    -p "./dist"
+    -p $DIST
 }
 
 setup_cache() {
   echo "Install dependencies for moira-cache"
-  $GOROOT/bin/go get -v "github.com/moira-alert/cache"
-  $GOROOT/bin/go install -v "github.com/moira-alert/cache"
+  $GOBIN get -v "github.com/moira-alert/cache"
+  $GOBIN install -v "github.com/moira-alert/cache"
 }
 
 package_cache() {
@@ -132,21 +165,21 @@ package_cache() {
   CACHE_DATE_COMMIT=$(date -d $(git --git-dir $GOPATH/src/github.com/moira-alert/cache/.git log --date=short --pretty=format:"%cd" -n 1) +%Y%m%d)
   CACHE_VERSION="$NOTIFIER_DATE_COMMIT$NOTIFIER_COMMIT"
 
-  mkdir -p ./package/etc/init.d
-  mkdir -p ./package/etc/moira
-  mkdir -p ./package/usr/sbin
-  mkdir -p ./package/usr/lib/systemd/system
+  mkdir -p $TMP/package/etc/init.d
+  mkdir -p $TMP/package/etc/moira
+  mkdir -p $TMP/package/usr/sbin
+  mkdir -p $TMP/package/usr/lib/systemd/system
 
-  cp ./files/cache/$PT/init.d/moira-cache ./package/etc/init.d/moira-cache
-  cp ./files/cache/$PT/systemd/moira-cache.service ./package/usr/lib/systemd/system/moira-cache.service
-  cp ./files/config.yml ./package/etc/moira/config.yml
-  cp $GOPATH/bin/cache ./package/usr/sbin/moira-cache
+  cp ./files/cache/$PT/init.d/moira-cache $TMP/package/etc/init.d/moira-cache
+  cp ./files/cache/$PT/systemd/moira-cache.service $TMP/package/usr/lib/systemd/system/moira-cache.service
+  cp ./files/config.yml $TMP/package/etc/moira/config.yml
+  cp $GOPATH/bin/cache $TMP/package/usr/sbin/moira-cache
 
   echo "Create $PT package"
   fpm -t $PT \
     -s "dir" \
     --description "Moira Cache" \
-    -C ./package \
+    -C $TMP/package \
     --vendor "$VENDOR" \
     --url "$URL" \
     --license "$LICENSE" \
@@ -157,14 +190,14 @@ package_cache() {
     --config-files "/etc/moira/config.yml" \
     --config-files "/usr/lib/systemd/system/moira-cache.service" \
     --after-install "./files/notifier/$PT/postinst" \
-    -p "./dist"
+    -p $DIST
 }
 
 package_full() {
   echo "Package full"
 }
 
-export GOPATH="$(pwd)/gocode"
+#export GOPATH="$(pwd)/gocode"
 check
 clean
 
@@ -173,12 +206,8 @@ case "$MICROSERVICE" in
     clean_package
     setup_notifier
     case "$PACKAGETYPE" in
-      "deb")
-        package_notifier "deb"
-      ;;
-      "rpm")
-        package_notifier "rpm"
-      ;;
+      "deb") package_notifier "deb" ;;
+      "rpm") package_notifier "rpm" ;;
       "docker")
         package_notifier "rpm"
         echo "Generate Docker Image"
@@ -194,12 +223,8 @@ case "$MICROSERVICE" in
     clean_package
     setup_cache
     case "$PACKAGETYPE" in
-      "deb")
-        package_cache "deb"
-      ;;
-      "rpm")
-        package_cache "rpm"
-      ;;
+      "deb") package_cache "deb" ;;
+      "rpm") package_cache "rpm" ;;
       "docker")
         package_cache "rpm"
         echo "Generate Docker Image"
